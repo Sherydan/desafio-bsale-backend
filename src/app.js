12f484dea4ba2afaa9fb392d531659df91fe865b
Sherydan <tobar.luis.c@gmail.com>
2022-07-17 03:11:32 -0400

    added a setInterval using http to keep the connection alive

--- src/app.js
+++ src/app.js
@@ -1,13 +1,25 @@
 import express from "express";
 import morgan from "morgan";
 
+const app = express();
+
+// import http from "http" so i can keep alive the connection
+ const http = require("http");
+
+ // ping api endpont every second
+setInterval(() => {
+    http.get("http://localhost:4000/api/products");
+}
+, 2000);
+
+
 // Importing the routes
 import productsRoutes from "./routes/products.routes";
 
-const app = express();
 
 // settings
 app.set("port", process.env.PORT || 4000);
+
 
 // middlewares
 // we can use morgan to see the requests in the console
--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,10 +1,17 @@
 import getConnection from "./../database/database";
 
+
 const getProducts = async(req, res) => {
-    const connection = await getConnection();
-    const result = await connection.query("SELECT * FROM product");
-    console.log(result);
-    res.json(result);
+    try {
+        const connection = await getConnection();
+        const result = await connection.query("SELECT * FROM product");
+        console.log(result);
+        res.json(result);
+        
+    } catch (error) {
+        res.status(500);
+        res.send(error);
+    }
     
 }
 
