def1fe3d4bb9a2b9e6661c4c8b7bd72cba4e591f
Sherydan <tobar.luis.c@gmail.com>
2022-07-18 11:46:41 -0400

    changed imports

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { methods as productsController } from "./../controllers/products.controller";
+import { methods as productsController } from "./../controllers/products.controller.js";
 
 const router = Router();
 
