59484dcfca9fb6923f03f055c2141a91448d686d
Sherydan <tobar.luis.c@gmail.com>
2022-07-19 23:32:15 -0400

    added swagger endpoints definition

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -29,16 +29,105 @@ const router = Router();
  *              category:
  *                  type: integer
  *                  description: categorym id
- *  
- * 
- * 
- * 
+  *      Category:
+ *          type: object
+ *          properties:
+ *              id:
+ *                  type: integer
+ *                  description: category unique id
+ *              name:
+ *                  type: string
+ *                  description: category name
  */
 
-
+//get all products
+/**
+ * @swagger
+ * /api/products:
+ * get:
+ *  summary: return all products
+ *  tags: [Products]
+ *  responses:
+ *      200:
+ *          description: return all products
+ *          content: application/json
+ *              schema:
+ *                  type: array
+ *                  items:
+ *                      $ref: '#/components/schemas/Products'
+ */
 router.get("/", productsController.getProducts);
+
+//get products offers
+/**
+ * @swagger
+ * /api/products/offers:
+ * get:
+ *  summary: return products offers
+ *  tags: [Products]
+ *  responses:
+ *      200:
+ *          description: return products offers
+ *          content: application/json
+ *              schema:
+ *                  type: array
+ *                  items:
+ *                      $ref: '#/components/schemas/Products'
+ */
 router.get("/offers", productsController.getOffers);
+
+//get products by category
+/**
+ * @swagger
+ * /api/products/category/{id}:
+ * get:
+ *  summary: return products by given id
+ *  tags: [Products]
+ *  parameters:
+ *      -   in: path
+ *          name: id
+ *          schema:
+ *              type: integer
+ *          required: true
+ *          description: category id
+ *  responses:
+ *      200:
+ *          description: return products by given id
+ *          content: application/json
+ *              schema:
+ *                  type: array
+ *                  items:
+ *                      $ref: '#/components/schemas/Products'
+ *      404:
+ *          description: no products found for the given category id
+ */
 router.get("/category/:category", productsController.getProductsByCategory);
+
+//get products offers
+/**
+ * @swagger
+ * /api/products/search/{name}:
+ * get:
+ *  summary: return products by the given name
+ *  tags: [Products]
+ *  parameters:
+ *      -   in: path
+ *          name: name
+ *          schema:
+ *              type: string
+ *          required: true
+ *          description: product name
+ *  responses:
+ *      200:
+ *          description: return products by the given name
+ *          content: application/json
+ *              schema:
+ *                  type: array
+ *                  items:
+ *                      $ref: '#/components/schemas/Products'
+ *      404:
+ *          description: no products found for the given name
+ */
 router.get("/search/:name", productsController.getProductsByName);
 
 export default router;
