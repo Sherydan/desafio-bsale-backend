f13c3f6626df27b0335cebe8bfaf1cd72a2e073d
Sherydan <tobar.luis.c@gmail.com>
2022-07-19 22:19:29 -0400

    added swagger setup

--- src/app.js
+++ src/app.js
@@ -1,8 +1,31 @@
 import express from "express";
 // import morgan from "morgan";
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
+
+// swagger
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerSpec = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Desafio BSALE Api',
+            version: '1.0.0',
+            description: 'Desafio BSALE Api la cual permite realizar la gestión de una tienda online',
+        },
+        servers: [
+            {
+                url: 'https://desafio-bsale-backend.herokuapp.com/api-doc',
+                description: 'Production'
+            }
+        ]
+    },
+    apis: [`${path.join(__dirname, './routes/*.js')}`]
+};
+
 
 // import http from "http" so i can keep alive the connection
  const http = require("http");
@@ -23,11 +46,14 @@ app.set("port", process.env.PORT || 4000);
 
 
 // middlewares
+
 // we can use morgan to see the requests in the console
 // app.use(morgan("dev"));
+// we can use cors to allow the requests from other domains
 app.use(cors());
 
 // routes
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(swaggerJsdoc(swaggerSpec)));
 app.use("/api/products", productsRoutes);
 app.use("/api/products/offers", productsRoutes);
 app.use("/api/products/category/:category", productsRoutes);
