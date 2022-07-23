af73f5be02349272947408c20aecf0d03d46478b
Sherydan <tobar.luis.c@gmail.com>
2022-07-23 01:06:51 -0400

    fixed swagger documentation

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -80,56 +80,56 @@ router.get("/", productsController.getAllProducts);
  */
 router.get("/offers", productsController.getAllOffers);
 
-// // get products by category id
-// /**
-//  * @swagger
-//  * /api/products/category/{id}:
-//  *   get:
-//  *     summary: get products by given category id
-//  *     tags: [Products]
-//  *     parameters:
-//  *       - in : path
-//  *         name: id
-//  *         description: id of category
-//  *         schema:
-//  *           type: integer
-//  *         required: true
-//  *     responses:
-//  *       200:
-//  *         description: products by category id
-//  *         content:
-//  *           application/json:
-//  *             schema:
-//  *               $ref: '#/components/schemas/Products'
-//  *       400:
-//  *         description: no products found
-//  */
+// get products by category id
+/**
+ * @swagger
+ * /api/products/category/{id}:
+ *   get:
+ *     summary: get products by given category id
+ *     tags: [Products]
+ *     parameters:
+ *       - in : path
+ *         name: id
+ *         description: id of category
+ *         schema:
+ *           type: integer
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: products by category id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Products'
+ *       400:
+ *         description: no products found
+ */
 router.get("/category/:category", productsController.getProductsByCategory);
 
-// // get products by name
-// /**
-//  * @swagger
-//  * /api/products/search/{name}:
-//  *   get:
-//  *     summary: get products by given category id
-//  *     tags: [Products]
-//  *     parameters:
-//  *       - in : path
-//  *         name: name
-//  *         description: name of product
-//  *         schema:
-//  *           type: string
-//  *         required: true
-//  *     responses:
-//  *       200:
-//  *         description: products by name
-//  *         content:
-//  *           application/json:
-//  *             schema:
-//  *               $ref: '#/components/schemas/Products'
-//  *       400:
-//  *         description: no products found
-//  */
+// get products by name
+/**
+ * @swagger
+ * /api/products/search/{name}:
+ *   get:
+ *     summary: get products by given name
+ *     tags: [Products]
+ *     parameters:
+ *       - in : path
+ *         name: name
+ *         description: name of product
+ *         schema:
+ *           type: string
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: products by name
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Products'
+ *       400:
+ *         description: no products found
+ */
 router.get("/search/:name", productsController.getProductsByName);
 
 module.exports = router;
